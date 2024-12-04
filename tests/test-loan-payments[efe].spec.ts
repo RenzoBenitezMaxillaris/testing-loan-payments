cf1dd8d409029595462c479cef26a342106e8548
rbenites <renzo.benitez@ti.maxillaris.pe>
2024-12-04 18:25:43 -0500

    refactoring code testing

--- tests/test-loan-payments[efe].spec.ts
+++ tests/test-loan-payments[efe].spec.ts
@@ -4,26 +4,20 @@ test('test', async ({ page }) => {
   await page.goto('http://192.168.1.211:4200/auth/signin');
   await page.getByPlaceholder('Usuario').fill('jose.jara');
   await page.getByPlaceholder('Password').fill('123@maxi');
-  // await page.getByPlaceholder('Usuario').press('Tab');
   await page.getByRole('button', { name: 'Acceder' }).click();
-  await page.locator('div').filter({ hasText: 'Acceso al Sistema Verificando' }).nth(2).click();
-  await page.waitForSelector('app-nav-group');
   await page.locator('a:has-text("Procesos")').click();
-  await page.locator('a[href="/finance/loan-payments"]').click();
+  await page.locator('xpath=//a[@href="/finance/loan-payments"]').click();
   const paciente = 'MX124-001992';
   await page.fill('input[aria-autocomplete="list"]', paciente);
-  const opcionPaciente = page.locator('li[role="option"]:visible');
+  const opcionPaciente = page.locator('xpath=//li[@role="option"]');
   await opcionPaciente.click();
   await page.locator('button:has-text("Buscar")').click();
   await page.waitForTimeout(3000);
-  await page.waitForSelector('table tbody');
-  const cuotasCargadas = await page.locator('table tbody td span.image-text').count();
-  console.log("cuotasCargadas: ", cuotasCargadas);
-  // await page.pause();
+  const data = await page.locator('table tbody td span.image-text').count();
+  console.log("cuotasCargadas: ", data);
 
-  if (cuotasCargadas) {
-    // const checkbox = page.locator('tbody tr input[type="checkbox"][ng-reflect-model="false"]:not(:disabled)').first();
-    const checkbox = page.locator('tbody tr input[type="checkbox"]:not(:disabled)');
+  if (data) {
+    const checkbox = page.locator("xpath=//input[@type='checkbox' and not(@disabled)]");
     console.log("checkbox: ", checkbox);
     try {
       await checkbox.first().waitFor({ state: 'visible', timeout: 5000 });
@@ -37,18 +31,15 @@ test('test', async ({ page }) => {
     console.log("Checkbox visible:", isCheckboxVisible);
 
     if (isCheckboxVisible) {
-      await checkbox.first().check();  
-      
-    // await page.pause();
+      await checkbox.first().check(); 
     } else {
       console.log("El checkbox no es visible");
     }
 
   } else {
     console.log("No se encontraron cuotas para pagar");
-    // await page.pause();
   }
-  await page.locator('button.btn.btn-sm.btn-success.mt-4.ml-1').nth(0).click();
+  await page.locator('button.btn.btn-sm.btn-success.mt-4.ml-1').nth(0).click(); //Pendiente colocar id
   
 
   const dataRow = page.locator('tr.pointer-modal');
@@ -56,35 +47,38 @@ test('test', async ({ page }) => {
 
   const today = new Date();
   const todayString = today.toISOString().split('T')[0];
-  await page.locator('input[type="date"]#PaymentDate').fill(todayString);
+  await page.locator("xpath=//input[@id='PaymentDate']").fill(todayString);
   await page.waitForTimeout(1000);
-  await page.locator('#ExchangeRateDay').selectOption({ index: 1 });
+  await page.locator("xpath=//select[@id='ExchangeRateDay']").selectOption({ index: 1 });
   await page.waitForTimeout(1000);
-  await page.locator('#PaymentMethod').selectOption({ index: 0 });
+  await page.locator("xpath=//select[@id='PaymentMethod']").selectOption({ index: 0 });
   await page.waitForTimeout(1000);
-  await page.locator('#DocumentSerie').selectOption({ index: 0 });
+  await page.locator("xpath=//select[@id='DocumentSerie']").selectOption({ index: 0 });
   await page.waitForTimeout(1000);
-  await page.locator('input[type="date"]#DocumentDate').fill(todayString);
+  await page.locator("xpath=//input[@id='DocumentDate']").fill(todayString);
   await page.waitForTimeout(1000);
-  await page.locator('input[name="description"]').fill('Texto de prueba automatizacion');
+  await page.locator("xpath=//input[@name='description']").fill('Texto de prueba automatizacion');
   await page.waitForTimeout(1000);
   const path = require('path');
   const filePath = path.join('C:', 'Users', 'user', 'Documents', 'ImageTesting.jpg');
   await page.waitForTimeout(3000);
-  await page.locator('input[type="file"]').setInputFiles(filePath);
+  await page.locator("xpath=//input[@type='file']").setInputFiles(filePath);
   await page.waitForTimeout(1000);
   const valueUnitInput = await page.locator("xpath=//input[@id='AttentionAmount']");
   const value = await valueUnitInput.inputValue();
   if (parseFloat(value) > 0) {
-    await page.locator('#btn-asignar').click();
+    await page.locator("xpath=//button[@id='btn-asignar']").click();
+    
+    await page.pause();
   }else {
     console.log("valor negativo");
     await valueUnitInput.fill('1');
     await page.waitForTimeout(1000);
+    await page.pause();
     await page.locator('#btn-asignar').click();
     await page.pause();
   }
-  await page.locator('#guarda-facturacion').click();
+  await page.locator("xpath=//button[@id='guarda-facturacion']").click();
   
   await page.pause();
 });
